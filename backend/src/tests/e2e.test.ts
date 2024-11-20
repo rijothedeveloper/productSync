d1f618959b3505dd6d61d782ce707aef9dd25aac
rijogeorge <rijothedeveloper@gmail.com>
2024-11-19 17:55:56 -0800

    integration test for register user

---
+++ backend/src/tests/e2e.test.ts
@@ -0,0 +1,16 @@
+import request from "supertest";
+import app from "../app";
+import { describe } from "node:test";
+
+describe("auth api tests", () => {
+  it("should create one user", async () => {
+    const res = await request(app).post("/auth/register").send({
+      name: "George",
+      userName: "George7372test",
+      email: "george737test@gmail.com",
+      phone: "5102365874",
+      password: "pass",
+    });
+    expect(res.status).toBe(201);
+  });
+});
