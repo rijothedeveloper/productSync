a3a5630ee32b832c3c8a92e5e0f8ca7c34896578
rijogeorge <rijothedeveloper@gmail.com>
2024-11-30 17:39:50 -0800

    user registeration edge cases added

--- backend/src/services/authentication.service.ts
+++ backend/src/services/authentication.service.ts
@@ -2,10 +2,63 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-let response: { error: any; statusCode: number; result: object | null };
+let response: { error: any; statusCode: number; data: object | null };
 
 export async function registerUser(user: User) {
   try {
+    if (!user.userName || !user.email || !user.password) {
+      response = {
+        error: "userName, email and password are required",
+        statusCode: 400,
+        data: null,
+      };
+      return response;
+    }
+    if (user.password.length < 6) {
+      response = {
+        error: "password length should be greater than 6",
+        statusCode: 400,
+        data: null,
+      };
+      return response;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!user.email.match(emailRegex)) {
+      response = {
+        error: "email is not in correct format",
+        statusCode: 400,
+        data: null,
+      };
+      return response;
+    }
+
+    const userNameExists = await prisma.users.findUnique({
+      where: {
+        userName: user.userName,
+      },
+    });
+    if (userNameExists) {
+      response = {
+        error: "userName already exists",
+        statusCode: 400,
+        data: null,
+      };
+      return response;
+    }
+    const emailExists = await prisma.users.findUnique({
+      where: {
+        email: user.email,
+      },
+    });
+    if (emailExists) {
+      response = {
+        error: "email already exists",
+        statusCode: 400,
+        data: null,
+      };
+      return response;
+    }
     const newUser = await prisma.users.create({
       data: {
         name: user.name,
@@ -16,9 +69,9 @@ export async function registerUser(user: User) {
         password: user.password,
       },
     });
-    response = { error: null, statusCode: 201, result: newUser };
+    response = { error: null, statusCode: 201, data: newUser };
   } catch (error) {
-    response = { error, statusCode: 500, result: null };
+    response = { error, statusCode: 500, data: null };
   }
   return response;
 }
--- backend/src/tests/e2e.test.ts
+++ backend/src/tests/e2e.test.ts
@@ -12,12 +12,83 @@ describe("auth api tests", () => {
       userName: "George7372test",
       email: "george737test@gmail.com",
       phone: "5102365874",
-      password: "pass",
+      password: "pass22",
     });
     expect(res.status).toBe(201);
   });
+  test("should return error 400 if user name is not passed in body", async () => {
+    const res = await request(app).post("/auth/register").send({
+      name: "George",
+      email: "george737test@gmail.com",
+      phone: "5102365874",
+      password: "pass22",
+    });
+    // expect(res.status).toBe(400);
+  });
 
-  afterEach(async () => {
+  test("should return error 400 if password length is < 6", async () => {
+    const res = await request(app).post("/auth/register").send({
+      name: "George",
+      userName: "George7372test",
+      phone: "5102365874",
+      password: "pass",
+    });
+    expect(res.status).toBe(400);
+  });
+
+  test("should return error 400 if email is not in correct format", async () => {
+    const res = await request(app).post("/auth/register").send({
+      name: "George",
+      userName: "George7372test",
+      phone: "5102365874",
+      password: "pass",
+    });
+    expect(res.status).toBe(400);
+  });
+
+  test("should return error 400 if password is not passed in body", async () => {
+    const res = await request(app).post("/auth/register").send({
+      name: "George",
+      userName: "George7372test",
+      email: "george737test@gmail.com",
+      phone: "5102365874",
+      password: "pass",
+    });
+    expect(res.status).toBe(400);
+  });
+  test("should return error 400 if password is not passed in body", async () => {
+    const res = await request(app).post("/auth/register").send({
+      name: "George",
+      userName: "George7372_2test",
+      email: "george737test2@gmail.com",
+      phone: "5102365874",
+    });
+    expect(res.status).toBe(400);
+  });
+
+  test("should return error 400 if user name already exists", async () => {
+    const res = await request(app).post("/auth/register").send({
+      name: "George",
+      userName: "George7372test",
+      email: "george737test2@gmail.com",
+      phone: "5102365874",
+      password: "pass",
+    });
+    expect(res.status).toBe(400);
+  });
+
+  test("should return error 400 if email already exists", async () => {
+    const res = await request(app).post("/auth/register").send({
+      name: "George",
+      userName: "George7372test",
+      email: "george737test2@gmail.com",
+      phone: "5102365874",
+      password: "pass",
+    });
+    expect(res.status).toBe(400);
+  });
+
+  afterAll(async () => {
     await prisma.users.delete({
       where: {
         userName: "George7372test",
