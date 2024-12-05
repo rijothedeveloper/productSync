b89c867f9a0d27e25a1412ff885084f97e164bc0
rijogeorge <rijothedeveloper@gmail.com>
2024-12-05 10:20:26 -0800

    workflow file changed reflecting new folder structure

--- backend/prisma/migrations/20241201004517_init3/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `business_name` on the `Users` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Users" DROP COLUMN "business_name",
-ADD COLUMN     "busines_name" TEXT;
